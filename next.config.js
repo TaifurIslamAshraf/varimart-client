1d10a44a24e6d7d19a8638dbdaba61d103aea5d3
Md Taifur <taifurislamashraf@gmail.com>
2024-08-31 19:24:27 +0600

    change next.config.ts

--- next.config.js
+++ next.config.js
@@ -2,38 +2,40 @@
 const nextConfig = {
   async headers() {
     return [
-        {
-            // Apply headers to all routes
-            source: '/(.*)',
-            headers: [
-                {
-                    key: 'Access-Control-Allow-Credentials',
-                    value: 'true',
-                },
-                {
-                    key: 'Access-Control-Allow-Origin',
-                    value: 'https://web-production-b912.up.railway.app',
-                },
-                {
-                    key: 'Access-Control-Allow-Methods',
-                    value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-                },
-                {
-                    key: 'Access-Control-Allow-Headers',
-                    value:
-                        
-'X-CSRF-Token, X-Requested-With, Accept, Accept- Version, Content - Length, Content - MD5, Content - Type, Date, X - Api - Version',
-                },
-            ],
-        },
+      {
+        // Apply headers to all routes
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://web-production-b912.up.railway.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept- Version, Content - Length, Content - MD5, Content - Type, Date, X - Api - Version",
+          },
+        ],
+      },
     ];
-},
-    images:{
-        remotePatterns:[{
-            hostname: "server.varimartbd.com"
-        }, {hostname: "localhost"}, {hostname: "web-production-5dac.up.railway.app"}]
-    },
-   
-}
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "server.varimartbd.com",
+      },
+      { hostname: "localhost" },
+      { hostname: "web-production-5dac.up.railway.app" },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
